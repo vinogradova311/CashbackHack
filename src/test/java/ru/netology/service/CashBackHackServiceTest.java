e7801ba8cce52fc98fbc56877813f2b580b3d643
vinogradova311 <anny.amelie@gmail.com>
2022-11-12 18:54:40 +0300

    testng commit

---
+++ src/test/java/ru/netology/service/CashBackHackServiceTest.java
@@ -0,0 +1,40 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+
+
+    public class CashBackHackServiceTest {
+        @Test
+        public void shouldRemain100() {
+            CashBackHackService bonusService = new CashBackHackService();
+
+            int actual = bonusService.remain(900);
+            int expected = 100;
+
+            assertEquals(actual, expected);
+        }
+
+        @Test
+        public void shouldRemain900() {
+            CashBackHackService bonusService = new CashBackHackService();
+
+            int actual = bonusService.remain(1100);
+            int expected = 900;
+
+            assertEquals(actual, expected);
+        }
+
+        @Test
+        public void whatIf1000() {
+            CashBackHackService bonusService = new CashBackHackService();
+
+            int actual = bonusService.remain(1000);
+            int expected = 0;
+
+            assertEquals(actual, expected);
+        }
+
+    }
